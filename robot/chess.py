f6313815979bb56b0ccaebae5038397a87faaf86
jonathan-dilorenzo <jonathan.dilorenzo@gmail.com>
2015-08-19 03:37:30 +0100

    Saving robot/chess.py

--- robot/chess.py
+++ robot/chess.py
@@ -12,12 +12,7 @@ s=5
 n=8
 
 for i in range(0,n):
-  for j in range(0,4):
+  for j in range(0,5):
     for k in range(0,s-1):
       move()
     turn(1)
-
-for i in range(0,10):
-  move()
-  #turn(360.0/r)
-  #color(colors[i%3])
