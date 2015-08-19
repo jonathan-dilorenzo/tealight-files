9319b6e6041c196d7f90a788e267ffb270f039aa
jonathan-dilorenzo <jonathan.dilorenzo@gmail.com>
2015-08-19 04:28:44 +0100

    Running robot/search.py

--- robot/search.py
+++ robot/search.py
@@ -8,8 +8,27 @@ from tealight.robot import (move,
 
 # This is a fairly useless algorithm!
 
+x=0
+
 while True:
-  move()
-  
-  if touch() == "wall":
-    turn(2)
+  if touch() == "fruit":
+    x=0
+    move()
+  elif right_side() == "fruit":
+    x=0
+    turn(1)
+    move()
+  elif left_side() == "fruit":
+    x=0
+    turn(3)
+    move()
+  elif look() == "fruit":
+    x=0
+    move()
+  elif x < 3:
+    x=x+1
+    turn(1)
+  else:
+    x=x+1
+    move()
+    turn(x%5)
