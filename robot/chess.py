96341ff27fcf53dabe8063a8ab4cd81e5f0d289a
jonathan-dilorenzo <jonathan.dilorenzo@gmail.com>
2015-08-19 03:36:30 +0100

    Saving robot/chess.py

--- robot/chess.py
+++ robot/chess.py
@@ -7,6 +7,16 @@ from tealight.robot import (move,
                             right_side)
 
 # Add your code here
+
+s=5
+n=8
+
+for i in range(0,n):
+  for j in range(0,4):
+    for k in range(0,s-1):
+      move()
+    turn()
+
 for i in range(0,10):
   move()
   #turn(360.0/r)
