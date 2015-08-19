f558a38dda412f348244beb59306c474430cbb07
jonathan-dilorenzo <jonathan.dilorenzo@gmail.com>
2015-08-19 04:02:31 +0100

    Running robot/cleanup.py

--- robot/cleanup.py
+++ robot/cleanup.py
@@ -6,4 +6,27 @@ from tealight.robot import (move,
                             left_side, 
                             right_side)
 
-# Add your code here
+x=0
+
+while True:
+  if touch() == "fruit":
+    x=0
+    move()
+  elif right_side() == "fruit":
+    x=0
+    turn(1)
+    move()
+  elif left_side() == "fruit":
+    x=0
+    turn(3)
+    move()
+  elif look() == "fruit":
+    x=0
+    move()
+  elif x < 3:
+    x=x+1
+    turn(1)
+  else:
+    x=x+1
+    move()
+    turn(x%5)
