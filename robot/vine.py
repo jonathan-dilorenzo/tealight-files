359e07f8048619de84f8f3f138f1128dca34b03b
jonathan-dilorenzo <jonathan.dilorenzo@gmail.com>
2015-08-19 04:26:18 +0100

    Running robot/vine.py

--- robot/vine.py
+++ robot/vine.py
@@ -8,3 +8,28 @@ from tealight.robot import (move,
 
 # Add your code here
 
+
+x=0
+
+while True:
+  if touch() == "fruit":
+    x=0
+    move()
+  elif right_side() == "fruit":
+    x=0
+    turn(1)
+    move()
+  elif left_side() == "fruit":
+    x=0
+    turn(3)
+    move()
+  elif look() == "fruit":
+    x=0
+    move()
+  elif x < 3:
+    x=x+1
+    turn(1)
+  else:
+    x=x+1
+    move()
+    turn(x%5)
